28e170b633fb97269c7ba6c5c8cdb91839a3523a
EmmadAlsalam <143703288+EmmadAlsalam@users.noreply.github.com>
2024-05-13 21:53:23 +0200

    GenerateVerifactionsCode är klar

---
+++ VerificationProvider/Data/Context/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerificationProvider.Data.Entities;
+
+namespace VerificationProvider.Data.Context;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{ public DbSet<VerificationRequestEntity> VerificationRequests { get; set; }
+}
